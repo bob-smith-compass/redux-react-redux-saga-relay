17cb4c82a0b1413b19d4f3130d8c1f2f882060f3
David <david.shams@gmail.com>
2019-09-21 12:48:04 -0500

    Container Row Col

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Buttton from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ThemedButton from './components/themed-button/ThemedButton';
 
 
 function App(props) {
 
   const handleClick = (e) => {
-    if(e.target.innerHTML==="+") {
+    if (e.target.innerHTML === "+") {
       console.log(`Action: ${'INCREASE'}`);
-      props.store.dispatch({type: 'INCREASE'});
-    } else if(e.target.innerHTML==="-") {
+      props.store.dispatch({ type: 'INCREASE' });
+    } else if (e.target.innerHTML === "-") {
       console.log(`Action: ${'INCREASE'}`);
-      props.store.dispatch({type: 'DECREASE'});
+      props.store.dispatch({ type: 'DECREASE' });
     }
   };
 
   return (
     <>
-    <h3>Redux</h3>
-    <p>Redux State/Store {props.store.getState()}</p>
-    <Buttton onClick={handleClick} >+</Buttton>
-    <Buttton onClick={handleClick} >-</Buttton>
-    <ThemedButton onClick={handleClick} >+</ThemedButton>
-    
+      <Container>
+        <Row>
+          <Col>
+            <h3>Redux</h3>
+          </Col>
+          <Col>
+            <p>Redux State/Store {props.store.getState()}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Buttton onClick={handleClick} >+</Buttton>
+          </Col>
+          <Col>
+            <Buttton onClick={handleClick} >-</Buttton>
+          </Col>
+          <Col>
+            <ThemedButton onClick={handleClick} >+</ThemedButton>
+          </Col>
+        </Row>
+      </Container>
+
     </>
   );
 }
