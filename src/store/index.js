f1eee47c954f231cd52235225a5b5994583df4f9
David <david.shams@gmail.com>
2019-09-21 13:28:14 -0500

    diseases sample and thier sysmptos

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,36 @@
 import { createStore } from 'redux';
 
 /**
- * 
+ * Redux State
+ * [https://www.nhsinform.scot/illnesses-and-conditions/a-to-z](https://www.nhsinform.scot/illnesses-and-conditions/a-to-z)
  */
-let defaultState = 0;
+const symptoms = [
+    `dizziness`,
+    `sweaty and clammy skin`,
+    `rapid heartbeat (tachycardia)`,
+    `shortness of breath`,
+    `feeling faint`,
+    `loss of consciousness`,
+];
+let defaultState = {
+    diseases: [
+        {name: 'cancer', description: 'Killer disease', symptoms: ['ra']},
+        {
+            name: 'Abdominal aortic aneurysm', 
+            description: 'An abdominal aortic aneurysm (AAA) is a swelling (aneurysm) of the aorta – the main blood vessel that leads away from the heart, down through the abdomen to the rest of the body', 
+            symptoms: [
+                `a pulsating feeling in your stomach (abdomen), usually near your belly button, that's usually only noticeable when you touch it`,
+                `persistent back pain`,
+                `persistent abdominal pain`,
+                symptoms[0],
+                symptoms[1],
+                symptoms[2],
+                symptoms[3],
+                symptoms[4],
+                symptoms[5]
+            ]}
+    ]
+};
 let reducer = (state = defaultState , action) => {
     console.log(state);
     switch(action.type) {
