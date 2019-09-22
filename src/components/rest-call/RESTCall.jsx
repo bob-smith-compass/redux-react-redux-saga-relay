fb49f31f7d2fe7519cfd7f6424a8d50b7f4057bd
David <david.shams@gmail.com>
2019-09-22 13:00:00 -0500

    BUG fixed: Uncaught (in promise) TypeError: Failed to execute 'json' on 'Response': body stream is locked
    res.clone().json()

--- src/components/rest-call/RESTCall.jsx
+++ src/components/rest-call/RESTCall.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+const fetch = require('node-fetch');
 
 const url = 'https://restcountries.eu/rest/v2/all';
 
 const RESTCall = () => {
-    const getData = (url) => {
+    let ldata = 'not yet';
+    const getData = () => {
+        fetch('https://restcountries.eu/rest/v2/all').then( (res) => {
+            console.log(res);
+            ldata = res.clone().json();
+            console.log(ldata);
+        });
+        /**
+         * Streams
+         * Node way
+         */
+        // fetch('https://restcountries.eu/rest/v2/all'
+        // //   "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
+        // ).then(res => {
+        //   const dest = fs.createWriteStream("./octocat.png");
+        //   res.body.pipe(dest);
+        // });
+        fetch("https://api.github.com/users/github")
+          .then(res => res.clone().json())
+          .then(json => console.log(json));
+    };
 
-    }
     return (
         <div>
             {url}
+            <div>{ldata}</div>
+            <button onClick={getData}>fetch data</button>
         </div>
     );
 }
